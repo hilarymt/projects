71dd9c20d4c81129a17d0bffcee7fb13fb40328c
Hilary Torres <hilarytorres@Hilarys-Air.lan>
2024-04-15 23:33:58 -0500

    comment improvements

--- python_projects/food_budget.py
+++ python_projects/food_budget.py
@@ -1,21 +1,16 @@
-# This is a simple food_budget function _v1:
+# Copy and paste this very simple piece of code into a python terminal
+# to calculate your weekly food expenses. 
 
 
 def food_budget(goal, grocery, take_out):
+# This simple function will return a comment and value based upon 
+# a users weekly food budget goal and weekly grocery and takeout expenses.
     if int(grocery) + int(take_out) > int(goal):
         return 'You are over your weekly food budget : $' + str(int(goal) - (int(grocery) + int(take_out)))
     elif int(grocery) + int(take_out) == int(goal):
         return 'You have reached your weekly food budget goal of : $' + str(int(goal))
     else:
         return 'Great job! You are within your weekly food budget : $' + str(int(goal) - (int(grocery) + int(take_out)))
-
-# The three inputs are weekly food cost goal, weekly grocery costs and weekly takeout costs that
-# our input by the user.
-
-# The first statement is the sum of the users weekly grocery and takeout costs, if this
-# is less than the weekly goal. The user is told they are within the weekly goal and difference
-# of the goal and sum of the grocery and takeout is displayed to user
-# The second statement
 
 
 goal = input("Please enter your weekly food budget goal: $ ")
@@ -24,3 +19,7 @@ take_out = input("Please enter your take out expenses for the week: $ ")
 
 
 print(food_budget(goal, grocery, take_out))
+
+
+# This is the beginning of a python engineering project, future iterations 
+# will improve on this very simple function. (4/15/24)
